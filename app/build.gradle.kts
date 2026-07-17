3516971738a71dfe356550f990e8b3eff8d0cfc1
pang <2803834833@qq.com>
2026-07-17 22:37:06 +0800

    chore: bump to v1.3.2 for update flow demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pang.mdreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
